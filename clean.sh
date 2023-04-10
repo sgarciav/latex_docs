ff947232b876e23866a7eab836e907b3db9d5c96
Sergio Garcia-Vergara <sergiodotgarcia@gmail.com>
2023-04-10 13:05:04 -0400

    apparently I missed some extensions

--- clean.sh
+++ clean.sh
@@ -13,6 +13,11 @@ find . -name "*.aux" -o -name "*.log" -o \
      -name "*.dvi" -o \
      -name "*.tmp" -o \
      -name "*.xref" -o \
+     -name "*.4ct" -o \
+     -name "*.css" -o \
+     -name "*.html" -o \
+     -name "*.idv" -o \
+     -name "*.lg" -o \
      -name "#*" -o \
      -name ".#*" -o \
      -name "_region_.tex" -o \
